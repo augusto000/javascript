2d91933a1f06ee2b61d14304d6500028a2589b53
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-05-06 16:51:33 -0300

    Loaded js file for debugging

---
+++ pruebaObj.js
@@ -0,0 +1,57 @@
+const Objeto={
+    productos:"zapatos",
+    cantidad:"4",
+    sucursal:[
+        {codSucursal:"12ad",
+         nombreSucursal:"Lomas",
+         Domicilio:"Av Belgrano 334",
+         email:"lomas32@gmail.com",
+         empleados:33,         
+       },
+       {codSucursal:"13aw",
+       nombreSucursal:"Tigre",
+       Domicilio:"Av Cazón 2831",
+       email:"tirgre89@gmail.com", 
+       empleados:6,        
+       },
+    ],
+    aumentoEmpleado: function(sucursal){
+        const indexSucursal = this.sucursal.findIndex(
+            (sucur)=> sucur.codSucursal == sucursal
+        );
+        if (indexSucursal>-1){
+             const file=document.querySelector('#fila')
+             console.log("Empleados antes: ",this.sucursal[indexSucursal].empleados++);
+             console.log("Empleados actuales :",this.sucursal[indexSucursal].empleados);
+             renderSucursal(Objeto.sucursal);
+        }
+        else {
+            console.log("not found!!");
+        }
+    }
+};
+
+function renderSucursal(sucursal){
+    const html_ = sucursal.map((sucurs)=>`<tr>
+                             <td>${sucurs.codSucursal}</td>
+                             <td>${sucurs.nombreSucursal}</td>
+                             <td>${sucurs.Domicilio}</td>
+                             <td>${sucurs.email}</td>
+                             <td>${sucurs.empleados}</td>
+                             </tr>`
+    );
+  document.getElementById('fila-item').innerHTML=html_.join("");
+}
+    const subBtn = document.querySelector('#btn');
+    subBtn.addEventListener("click", function(){
+        const inputSucursal = document.querySelector('#inputSucursal').value;
+        Objeto.aumentoEmpleado(inputSucursal);
+    });    
+
+
+
+
+
+
+
+
